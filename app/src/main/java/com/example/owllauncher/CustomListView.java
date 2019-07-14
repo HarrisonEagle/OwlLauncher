4d40fd2b9ba10f8d3cdb24d57a62f8fa28bba981
Hidetomo8111F <hyzitbios@hotmail.com>
2019-07-14 14:26:15 +0900

    Initial commit

---
+++ app/src/main/java/com/example/owllauncher/CustomListView.java
@@ -0,0 +1,76 @@
+package com.example.owllauncher;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class CustomListView extends BaseAdapter {
+
+    Context context;
+    LayoutInflater layoutInflater = null;
+    List<ResolveInfo> pkgAppList;
+
+    public CustomListView(Context context) {
+        this.context = context;
+        this.layoutInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    public void setTweetList(List<ResolveInfo> pkgAppList) {
+        this.pkgAppList = pkgAppList;
+    }
+
+    @Override
+    public int getCount() {
+        return pkgAppList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return pkgAppList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        convertView = layoutInflater.inflate(R.layout.app_list,parent,false);
+
+        ((TextView)convertView.findViewById(R.id.appname)).setText(MainActivity.appnamelist.get(position));
+        ((ImageView)convertView.findViewById(R.id.appicon)).setImageDrawable(MainActivity.iconres.get(position));
+        ((LinearLayout)convertView.findViewById(R.id.openapp)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ActivityInfo activity=MainActivity.pkgAppsList.get(position).activityInfo;
+                ComponentName name=new ComponentName(activity.applicationInfo.packageName,
+                        activity.name);
+                Intent i=new Intent(Intent.ACTION_MAIN);
+
+                i.addCategory(Intent.CATEGORY_LAUNCHER);
+                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK |
+                        Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
+                i.setComponent(name);
+
+                view.getContext().startActivity(i);
+            }
+        });
+
+
+        return convertView;
+    }
+
+}
---
+++ app/src/main/java/com/example/owllauncher/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.owllauncher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static List<ResolveInfo> pkgAppsList;
+    public static List<String> appnamelist;
+    public static List<Drawable> iconres;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        PackageManager pm = this.getPackageManager();
+
+
+
+        Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
+        mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+        pkgAppsList =getApplicationContext().getPackageManager().queryIntentActivities( mainIntent, 0);
+        appnamelist = new ArrayList<String>();
+        iconres = new ArrayList<Drawable>();
+
+        for(int i=0;i<pkgAppsList.size();i++){
+            appnamelist.add(pkgAppsList.get(i).loadLabel(pm).toString());
+
+        }
+        for(int i=0;i<pkgAppsList.size();i++){
+            iconres.add(pkgAppsList.get(i).loadIcon(pm));
+
+        }
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        CustomListView listView=new CustomListView(MainActivity.this);
+        listView.setTweetList(pkgAppsList);
+        ListView applist=findViewById(R.id.applist);
+        applist.setAdapter(listView);
+
+
+
+
+    }
+}
